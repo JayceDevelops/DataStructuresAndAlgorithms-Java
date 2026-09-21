dad3d16c774e2fdecb0f4b9a125c3b9e6d8ac1cb
JayceDevelops <JayceIezzi@gmail.com>
2026-09-21 18:27:12 -0400

    Finished assignment 1

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,24 +1,37 @@
 public class QuickSort {
-    public QuickSort(int[] arr, int low, int high){
-        if (low >= high){
-            return; // array of 1 is sorted
+    public QuickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return; 
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
         }
 
         int pivot = arr[high];
+        int counter = low;
 
-        int counter = low; // Where to put the pivot
-
-        for (int i = low; i < high; i++){
-            if (arr[i] < pivot){
-                Sorting.swap(arr, i, counter);
+        for (int i = low; i < high; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, i, counter);
                 counter++;
             }
         }
 
-        // insert pivot in correct spot
-        Sorting.swap(arr, counter, high);
+        
+        swap(arr, counter, high);
 
-        QuickSort(arr, low, counter - 1); // Left Side
-        QuickSort(arr, counter + 1, high); // Right side
+        quickSort(arr, low, counter - 1);   // left side
+        quickSort(arr, counter + 1, high);  // right side
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
--- Sorting/assignment1.java
+++ Sorting/assignment1.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,12 +10,17 @@ public class assignment1 {
         algos.put("selection", getTimes("selection"));
         algos.put("insertion", getTimes("insertion"));
         algos.put("merge", getTimes("merge"));
+        algos.put("quick", getTimes("quick"));
 
-
-        for (String key : algos.keySet()){
-            System.out.print(key + ": ");
-            printArray(algos.get(key));
-            System.out.println();
+        try (FileWriter writer = new FileWriter("Sorting/data.txt", true)){
+            for (String key : algos.keySet()){
+                writer.write(key + ": ");
+                writer.write(dataAsString(algos.get(key)));
+                writer.write('\n');
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error");
         }
     }
 
@@ -21,10 +28,10 @@ public class assignment1 {
 
         double[] times = new double[200];
 
-        for (int i = 1000; i <= 200_000; i += 1000){
+        for (int i = 100; i <= 10_000; i += 100){
             int[] arr = Sorting.getRandom(100000, i);
             double time = getTime(algo, arr);
-            times[(i / 1000) - 1] = time;
+            times[(i / 100) - 1] = time;
         }
 
         return times;
@@ -53,7 +60,7 @@ public class assignment1 {
             }
 
             case "quick" -> {
-
+                QuickSort sorted = new QuickSort(arr);
             }
         }
         
@@ -68,4 +75,18 @@ public class assignment1 {
         }
     }
 
+    private static String dataAsString(double[] arr){
+        String newStr = "";
+
+        for (int i = 0; i < arr.length; i++){
+            if (i < arr.length - 1){
+                newStr += arr[i] + ", ";
+            }
+            else {
+                newStr += arr[i];
+            }
+        }
+
+        return newStr;
+    }
 }
